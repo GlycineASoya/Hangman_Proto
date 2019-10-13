85a8fc7dcab38e8e02e9567e801c099e118571a6
Ilia Levinskii <ivlevinsky@gmail.com>
2019-10-13 18:26:59 +0300

    Third Commit

---
+++ hm_proto.py
@@ -0,0 +1,12 @@
+def check_letter(possible_letter: str) -> bool:
+    #check the letter if it is not a null and/or a string
+    return None
+
+
+print("This is the word game called HangMan")
+
+guessed_Word = input("Please, enter the word to guess: ")
+
+while True:
+    possible_letter = input("Enter possible letter: ")
+    check_letter(possible_letter)
